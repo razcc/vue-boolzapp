c70e9e242e6f7890fd6bbfd5cb7cb82b0276eda1
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-11-15 15:26:29 +0100

    Piccole modifiche

--- main.js
+++ main.js
@@ -2,27 +2,15 @@ var app = new Vue({
     el: "#root",
     data: {
         indiceCustom: 0,
+        indiceCustom2: 0,
+        variabileMesaggio: "",
         contacts: [
             {
                 name: 'Michele',
                 avatar: 'img/avatar_1.jpg',
                 visible: true,
                 messages: [
-                    {
-                        date: '10/01/2020 15:30:55',
-                        message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        message: 'Ricordati di stendere i panni',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 16:15:22',
-                        message: 'Tutto fatto!',
-                        status: 'received'
-                    }
+
                 ],
             },
             {
@@ -30,21 +18,8 @@ var app = new Vue({
                 avatar: 'img/avatar_2.jpg',
                 visible: true,
                 messages: [
-                    {
-                        date: '20/03/2020 16:30:00',
-                        message: 'Ciao come stai?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '20/03/2020 16:30:55',
-                        message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
-                    },
-                    {
-                        date: '20/03/2020 16:35:00',
-                        message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
-                    }
+
+
                 ],
             },
             {
@@ -52,21 +27,7 @@ var app = new Vue({
                 avatar: 'img/avatar_3.jpg',
                 visible: true,
                 messages: [
-                    {
-                        date: '28/03/2020 10:10:40',
-                        message: 'La Marianna va in campagna',
-                        status: 'received'
-                    },
-                    {
-                        date: '28/03/2020 10:20:10',
-                        message: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '28/03/2020 16:15:22',
-                        message: 'Ah scusa!',
-                        status: 'received'
-                    }
+
                 ],
             },
             {
@@ -74,16 +35,7 @@ var app = new Vue({
                 avatar: 'img/avatar_4.jpg',
                 visible: true,
                 messages: [
-                    {
-                        date: '10/01/2020 15:30:55',
-                        message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
-                    }
+
                 ],
             },
             {
@@ -91,16 +43,7 @@ var app = new Vue({
                 avatar: 'img/avatar_5.jpg',
                 visible: true,
                 messages: [
-                    {
-                        date: '10/01/2020 15:30:55',
-                        message: 'Ricordati di chiamare la nonna',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        message: 'Va bene, stasera la sento',
-                        status: 'received'
-                    }
+
                 ],
             },
             {
@@ -108,21 +51,8 @@ var app = new Vue({
                 avatar: 'img/avatar_6.jpg',
                 visible: true,
                 messages: [
-                    {
-                        date: '10/01/2020 15:30:55',
-                        message: 'Ciao Claudia, hai novità?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        message: 'Non ancora',
-                        status: 'received'
-                    },
-                    {
-                        date: '10/01/2020 15:51:00',
-                        message: 'Nessuna nuova, buona nuova',
-                        status: 'sent'
-                    }
+
+
                 ],
             },
             {
@@ -130,16 +60,8 @@ var app = new Vue({
                 avatar: 'img/avatar_7.jpg',
                 visible: true,
                 messages: [
-                    {
-                        date: '10/01/2020 15:30:55',
-                        message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                        status: 'received'
-                    }
+
+
                 ],
             },
             {
@@ -147,30 +69,30 @@ var app = new Vue({
                 avatar: 'img/avatar_8.jpg',
                 visible: true,
                 messages: [
-                    {
-                        date: '10/01/2020 15:30:55',
-                        message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                        status: 'received'
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 15:51:00',
-                        message: 'OK!!',
-                        status: 'received'
-                    }
+
                 ],
             }
         ]
     },
     methods: {
-        iniziaChat(contatti, index){
+        iniziaChat(contatti, index) {
             this.indiceCustom = index;
+            this.variabileMesaggio= "";
+        },
+        keyEvent() {           
+            this.timingFunction()
         },
 
+        timingFunction(){
+            setTimeout(function(){
+                this.contacts[this.indiceCustom].messages.push({
+                    date: "--",
+                    status: "sent",
+                    message: this.variabileMesaggio
+                });
+                console.log( this.contacts[this.indiceCustom].messages)
+            },1000)
+        },
     }
 })
-
+s
